db152a69cbafec6faca7fa44c3ee7b4b5926c4d2
Ayush jha <jhaa97204@gmail.com>
2025-02-25 11:06:34 +0530

    first commit

---
+++ basics/Data Types/string.py
@@ -0,0 +1,28 @@
+chai ="masala chai"
+# first_char = chai[0]
+# # print(first_char)
+# slice_chai = chai[0:6]
+# # print(slice_chai)
+
+
+# print(chai.upper())
+# chai = "Lemon chai"
+# print(chai.replace("Lemon","ginger"))
+
+
+# chai = "lemon, ginger, green, mint"
+# print(chai.split())
+# print(chai.split(", "))
+
+
+# print(chai.find("chai"))
+
+# print(chai.count("c"))
+
+# order = "i ordered {} cups of {} chai"
+# print(order.format(2, "ginger"))
+
+# chai = ["lemon", "ginger", "green", "mint"]
+# print("".join(chai))
+# print("--".join(chai))
+
