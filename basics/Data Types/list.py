24319d72ec3e48eeb692e23884b1a7ea88e7cbbb
Ayush jha <jhaa97204@gmail.com>
2025-02-25 11:27:59 +0530

    first commit

---
+++ basics/Data Types/list.py
@@ -0,0 +1,32 @@
+tea_varieties = ["black","green","oolang","white"]
+
+# print(tea_varieties)
+# print(tea_varieties[1])
+# print(tea_varieties[1:])
+
+# tea_varieties[3] = "herbal"
+# print(tea_varieties)
+
+
+# tea_varieties[1:1] = ["test","test"]
+# print(tea_varieties)
+# for tea in tea_varieties:
+#         print(tea, end="-")
+# if "white" in tea_varieties:
+#     print("white is in the list")
+
+
+# print(tea_varieties.pop())
+
+# tea_varieties.remove("green")
+
+# tea_varieties.insert(1, "oolang")
+
+
+# print(tea_varieties)
+
+
+cube_num = [x**3 for x in range(10)]
+print(cube_num)
+    
+    
