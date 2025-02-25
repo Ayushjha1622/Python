e86ce9ba4cdbd141fef9d23037306ddd818175ea
Ayush jha <jhaa97204@gmail.com>
2025-02-25 12:03:26 +0530

    first commit

---
+++ basics/Data Types/tuple.py
@@ -0,0 +1,13 @@
+tea_types = ("black","green","white")
+# print(tea_types)
+
+# print(len(tea_types))
+
+more_tea = ("herbal","earl grey")
+all_tea = more_tea + tea_types
+# print(all_tea)
+
+# if "green" in all_tea:
+#     print("Green tea is available")
+
+
