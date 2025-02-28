424f17f54372ec83827e0e59dc0f9097ac18675f
Ayush jha <jhaa97204@gmail.com>
2025-02-28 20:48:52 +0530

    first commit

--- OOPS/01_sol.py
+++ OOPS/01_sol.py
@@ -1,11 +1,53 @@
-class Car:
-   def __init__(self, brand, model):
-       self.brand = "BMW"
-       self.model = "M3"
+# class Car:
+#    def __init__(self, brand, model):
+#        self.brand = "BMW"
+#        self.model = "M3"
     
-my_car = Car("toyota","corolla")
-print(my_car.brand)
-print(my_car.model)
+# my_car = Car("toyota","corolla")
+# print(my_car.brand)
+# print(my_car.model)
 
-my_new_car = Car("tata","safari")
-print(my_new_car.model)
+# my_new_car = Car("tata","safari")
+# print(my_new_car.model)
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+        
+# p1=Person("Ayush",22)
+# print(p1.name)
+# print(p1.age)
+
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+        
+#     def __str__(self):
+#         return f"{self.name}({self.age})"
+    
+# p1=Person("Ayush",22)
+# # print(p1.name)
+# # print(p1.age)    
+
+# print(p1)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def myFunc(self):
+        print("hello my name is "+ self.name)
+        
+        
+p1 = Person("John", 36)
+p1.myFunc()
+        
+
+
+        
+        
